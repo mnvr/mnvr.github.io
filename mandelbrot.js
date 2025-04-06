83eec4f67c4f59e4fdfb59724c03fbe6ead8bdb6
Manav Rathi <manav@mnvr.in>
2025-04-06 21:28:16 +0530

    More notes

--- mandelbrot.js
+++ mandelbrot.js
@@ -3,8 +3,8 @@
 export const mbExit = ([cx, cy], iter = 100) => {
   let x = 0, y = 0;
   for (let i = 1; i <= iter; i++) {
-    let nx = x * x - y * y + cx
-    let ny = 2 * x * y + cy
+    const nx = x * x - y * y + cx
+    const ny = 2 * x * y + cy
     x = nx;
     y = ny;
     if (Math.abs(x) > 2 || Math.abs(y) > 2) return i;
